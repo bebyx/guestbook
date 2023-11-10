13f98d8f69df4f1004e0231293cdc0bea39340be
bebyx <artem@bebyx.co.ua>
2023-11-10 22:51:33 +0200

    Handle post request more precisely

--- src/controllers/crud/post.ts
+++ src/controllers/crud/post.ts
@@ -2,21 +2,27 @@ import pool from '../../db/pool.js';
 
 export default async (req: any, res: any) => {
     const body = req.body;
-    const username = body.name;
-    const email = body.email;
-    const message = body.message;
+    const username: string = body.name === "" ? "Anonymous" : body.name;
+    const email: string = body.email;
+    const message: string = body.message;
+
+    if (message === "") {
+	const errorMessage = "Message should not be empty.";
+	console.error(errorMessage);
+	return res.render('error', { errorMessage });
+    }
 
     try {
-        const insertQuery = `
+        const insertQuery: string = `
           INSERT INTO messages (username, email, message)
           VALUES ($1, $2, $3)
         `;
 
-        const values = [username, email, message];
+        const values: string[] = [username, email, message];
 
         const result = await pool.query(insertQuery, values);
 
-        console.log('Row inserted successfully:', result);
+        console.log('Row inserted successfully:\n', result);
     } catch (err) {
         console.error('Error executing insert query:', err);
     }
