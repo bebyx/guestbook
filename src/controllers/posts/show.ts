604714c0114663a36f04d6c6d6eeb53087e89989
bebyx <artem@bebyx.co.ua>
2023-11-11 14:04:00 +0200

    Fix typo

--- src/controllers/posts/show.ts
+++ src/controllers/posts/show.ts
@@ -3,7 +3,7 @@ import pool from '../../db/pool.js';
 export default async (req: any, res: any) => {
     const id = req.params.id;
     
-    const result = await pool.query(`SELECT * FROM messages WHERE id=$1}`, [id]);
+    const result = await pool.query(`SELECT * FROM messages WHERE id=$1`, [id]);
     console.log(result);
     const post = result.rows[0];
 
